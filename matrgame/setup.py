f8faf72a2c73fc77d0159e4ecee2f90ebb82609e
nickdenisov777 <nick_denisov_777@mail.ru>
2019-11-02 13:04:08 +0300

    Added package folder

---
+++ matrgame/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='matrgame',
+      version='0.1',
+      description='Find optimal strategies for players A and B',
+      url='https://github.com/r4ndompuff/Tupo_prak',
+      author='Pozhilaya salamandra',
+      author_email='flyingcircus@example.com',
+      license='MSU',
+      packages=['matrgame'],
+      install_requires=[
+            'numpy'
+            'itertools'
+            'math'
+            're'
+            'fractions'
+      ],
+      zip_safe=False)
