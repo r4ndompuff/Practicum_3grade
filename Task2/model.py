01c16b2a477e58726663889d708d3f923fdc4584
nickdenisov777 <nick_denisov_777@mail.ru>
2019-11-23 11:26:33 +0300

    Work in progress...

--- Task2/model.py
+++ Task2/model.py
@@ -5,4 +5,8 @@ import xlrd as xl
 
 print("the beginning of task2")
 
-training = xl.open_workbook("training.xlsx")
+#training = xl.open_workbook("training.xlsx")
+training = pd.read_excel('training.xlsx')
+training_matrix = training.to_numpy()
+print(training_matrix[0,0])
+print(training_matrix[0,1])
