6cd7b68578e7f454f530ee3a0776e1c9773fa67a
nickdenisov777 <nick_denisov_777@mail.ru>
2019-11-28 23:46:16 +0300

    In progress...

--- Task2/model.py
+++ Task2/model.py
@@ -3,49 +3,65 @@ import pandas as pd
 import statsmodels.api as sm
 import xlrd as xl
 import matplotlib.pyplot as plt
+import statistics as st
 
-def avg_data(df):
+def avg_data(df): #скользящая средняя
+    rows, columns = df.shape
     averages = []
     averages.append(df['Value'][0])
-    for i in range(1, (df.size)//2):
+    for i in range(1, rows):
         elem = 0
         for j in range(i):
-            #print(j)
-            #print(df['Value'][j])
             elem = elem + df['Value'][j]
         elem = elem/i
         averages.append(elem)
-    #print(averages[359])
     return averages
 
-def df_test(df):
-    values_avg = np.average(df['Value'].to_numpy())
-    variation = np.var(df['Value'].to_numpy())
-    variation_2 = (df.size//2)/(df.size//2 -2)
-    #print(variation)
-    t = values_avg/(((variation**2)/(df.size//2)))**(1/2)
+def white_noise(df): #первые разности
+    rows, columns = df.shape
+    noise = []
+    noise.append(df['Value'][0])
+    #print(df.size)
+    for i in range(1, rows):
+        noise.append(df['Value'][i]-df['Value'][i-1])
+    noise[0] = noise[1]
+    return noise
+
+def df_test(df): #типа тест Дики-Фуллера, но на самом деле хуйня
+    rows, columns = df.shape
+    #print(rows)
+    #values_avg = np.average(df['Value'].to_numpy())
+    #variation = np.var(df['Value'].to_numpy())
+    #variation_2 = (rows)/(rows -2)
+    #t = values_avg/(((variation**2)/(rows)))**(1/2) Закоменченное - какое-то странное число, нормальная формула ниже
+    avg = np.average(df['Value'].to_numpy()) #среднее зачение
+    mode = st.mode(df['Value']) #мода (хз, она ли должна быть в формуле)
+    sigma = 0 #сигма (в квадрате) из формулы
+    for i in range(rows):
+        sigma = sigma + (df['Value'][i] - avg)**2
+    sigma = sigma/(rows - 1)
+    sigma = sigma**(1/2)
+    t = (avg - mode)/(sigma*(rows**(1/2))) #сама формула
     return t
 
 # MAIN
 
-#training = xl.open_workbook("training.xlsx")
 training = pd.read_excel('training.xlsx')
 #print(training.columns) #названия столбов
-#print(training.index) #количество строк (считается с нуля)
-#print(training['Value'])
-#print(training.size)
-
 
 training['Average'] = avg_data(training) #добавляем новый столбец в наш dataframe
+training['Noise'] = white_noise(training) #добавляем новый столбец в наш dataframe
 
 stacked = plt.gca() #2 plots 1 figure
 
 training.plot(kind='line',x='Date',y='Value',ax=stacked)
 training.plot(kind='line',x='Date',y='Average',color='green',ax=stacked)
-#plt.show()
+training.plot(kind='line',x='Date',y='Noise',color='purple',ax=stacked)
+plt.show()
 
 print(df_test(training))
-print(sm.tsa.adfuller(training['Value']))
+#print(np.var(training['Noise'].to_numpy()))
+print(sm.tsa.adfuller(training['Value'])) #проверяем рабочесть нашего теста Дики-Фуллера на библиотечном
 
 
 #training_matrix = training.to_numpy()
