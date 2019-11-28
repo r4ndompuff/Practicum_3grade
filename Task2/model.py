587a5f55e74d525a50435a097287f6250baf500e
nickdenisov777 <nick_denisov_777@mail.ru>
2019-11-28 15:37:13 +0300

    In progress...

--- Task2/model.py
+++ Task2/model.py
@@ -6,13 +6,16 @@ import matplotlib.pyplot as plt
 
 def avg_data(df):
     averages = []
-    for i in df.index:
+    averages.append(df['Value'][0])
+    for i in range(1, (df.size)//2):
         elem = 0
-        for j in range(i+1):
+        for j in range(i):
+            #print(j)
+            #print(df['Value'][j])
             elem = elem + df['Value'][j]
-        elem = elem/(i+1)
+        elem = elem/i
         averages.append(elem)
-    #print(averages)
+    print(averages[359])
     return averages
 
 def df_test():
@@ -23,8 +26,11 @@ def df_test():
 
 #training = xl.open_workbook("training.xlsx")
 training = pd.read_excel('training.xlsx')
-print(training.columns) #названия столбов
-print(training.index) #количество строк (считается с нуля)
+#print(training.columns) #названия столбов
+#print(training.index) #количество строк (считается с нуля)
+#print(training['Value'])
+#print(training.size)
+
 
 training['Average'] = avg_data(training) #добавляем новый столбец в наш dataframe
 
@@ -34,7 +40,6 @@ training.plot(kind='line',x='Date',y='Value',ax=stacked)
 training.plot(kind='line',x='Date',y='Average',color='green',ax=stacked)
 plt.show()
 
-#print(training['Value'][5])
 training_matrix = training.to_numpy()
 #print(training_matrix[0,0])
 #print(training_matrix[0,1])
